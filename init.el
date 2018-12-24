6dd405b9bacea85a6a13c08f65b2845ca3b29d29
Nikita Sivakov <sivakov512@gmail.com>
2018-12-24 17:13:41 +0300

    Setup straight and use-package

--- init.el
+++ init.el
@@ -7,4 +7,9 @@
 ;;; Code:
 
 (set-frame-font "Roboto Mono-9")
+
+(add-to-list 'load-path "~/.emacs.d/conf"); my cinfiguration dir
+(require 'plugins); plugins settings
+
+(provide 'init)
 ;;; init.el ends here
